5f1bbf3a9642228c195c244f9315223b6930bc0a
Kjetil Valle <kjetil.valle@gmail.com>
2015-05-03 19:11:38 +0200

    Problem 12

--- src/Lists.hs
+++ src/Lists.hs
@@ -143,3 +143,18 @@ encodeModified = map foo . encode
 data Encoded a = Single a | Multiple Int a 
     deriving (Show)
 
+
+-- | Problem 12
+--
+-- Decode a run-length encoded list.
+--
+-- Given a run-length code list generated as specified in problem 11. 
+-- Construct its uncompressed version.
+--
+-- >>> decodeModified [Multiple 4 'a',Single 'b',Multiple 2 'c',Multiple 2 'a',Single 'd',Multiple 4 'e']
+-- "aaaabccaadeeee"
+decodeModified :: [Encoded a] -> [a]
+decodeModified = concatMap decode
+    where decode (Multiple n a) = (replicate n a)
+          decode (Single a) = [a]
+
